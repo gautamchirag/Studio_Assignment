e56ca1cde33619b3f2bf04ed23cee65ebb179f1b
Chirag_Gautam <109953915+gautamchirag@users.noreply.github.com>
2023-09-30 11:49:26 +0530

    Update App.jsx

--- shopkart-app/src/App.jsx
+++ shopkart-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Header from './componets/Header/Header'
 import Heroj from './componets/Hero/Heroj'
-import Product from './componets/Product/Product'
+import Product from './componets/product/Product'
 import Contact from './componets/contact/Contact'
 import Footer from './componets/fot/Footer'
 function App() {
